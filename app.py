5d3dda02650feef96ff3849221fcd1e524a1d9e2
david17cor <david.17.cor@gmail.com>
2026-03-25 20:36:01 -0300

    Aprendiendo a desplegar data con render

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+@app.route('/')
+def ping():
+    data = {
+        "code": 200,
+        "response": "Respuesta desde API en Docker"
+	}
+    return jsonify(data)
+
+
+@app.route('/productos')
+def productos():
+    data = df.to_dict(orient="records")
+
+    return jsonify({
+        "code" : 200,
+        "data" : data
+    })
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
